cf14a7fd4c2dd6020141e451838326c00362f3b1
Zhang feilong <Zhang.feilong1@foxmail.com>
2017-08-18 23:16:20 +0800

    注册也æé¡页面部分完成

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
-	
+//登录表单
+
+$(document).ready(function() {
+
+    $('.all form').submit(function(){
+        var realname = $(this).find('.realname').val();
+        var password = $(this).find('.password').val();
+        if(realname == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '27px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.realname').focus();
+            });
+            return false;
+        }
+        if(password == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '96px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password').focus();
+            });
+            return false;
+        }
+    });
+
+    $('.all form .realname, .all form .password').keyup(function(){
+        $(this).parent().find('.error').fadeOut('fast');
+    });
+
 });
---
+++ pages/js/register.js
@@ -0,0 +1,74 @@
+//注册表单
+
+$(document).ready(function() {
+
+    $('.all form').submit(function(){
+        var realname = $(this).find('.realname').val();
+        var loginname = $(this).find('.loginname').val();
+        var password2 = $(this).find('.password2').val();
+        var password = $(this).find('.password').val();
+        var department = $(this).find('select  option:selected').val();
+        if(realname == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '27px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.realname').focus();
+            });
+            return false;
+        }
+        if(loginname == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '96px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password').focus();
+            });
+            return false;
+        }
+        if(password == ''){
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '165px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password').focus();
+            });
+            return false;
+        }
+        if(password2 == ''){
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '234px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password2').focus();
+            });
+            return false;
+        }
+        if(department == ''){
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '303px');
+                alert("请选择平台！");
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('#department').focus();
+            });
+            return false;
+        }
+        if(password2 != password){
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '234px');
+                alert("两次输入得密码不相同！");
+            });
+
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password2').focus();
+            });
+            return false;
+        }
+    });
+
+    $('.all form .realname, .all form .loginname').keyup(function(){
+        $(this).parent().find('.error').fadeOut('fast');
+    });
+
+});
