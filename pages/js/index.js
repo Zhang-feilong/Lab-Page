91ed272ec0f63c04532d55e369080256b8996d84
Zhang feilong <Zhang.feilong1@foxmail.com>
2017-08-24 20:04:10 +0800

    h

--- pages/js/index.js
+++ pages/js/index.js
@@ -8,3 +8,53 @@ function add(){
 function remove(){
 	$(".nav ul li a").removeClass("next");
 }
+
+
+$("#btn").click(function(){
+	var xhr = createXHR();
+	xhr.open("POST", "api/file/upload", true);
+	xhr.onreadystatechange = function() {
+	if (xhr.readyState == 4 && xhr.status == 200) {
+	// Handle response.
+	alert(xhr.responseText); // handle response.
+	}
+	};
+	fd.append('myFile', file);
+	// Initiate a multipart/form-data upload
+	xhr.send(fd)
+});
+
+
+
+function c () {
+    var r= new FileReader();
+    f=document.getElementById('file').files[0];
+    r.readAsDataURL(f);
+    r.onload=function  (e) {
+        document.getElementById('img').src=this.result;
+    };
+}
+
+function createXHR(){
+	if(typeof XMLHttpRequest !="undefined"){
+		return new XMLHttpRequest();
+	}else if (typeof ActiveXObject !="undefined"){
+		if(typeof arguments.callee.activeXString !="string"){
+			var versions = ["MSXML2.XMLHttp.6.0","MSXML2.XMLHttp.3.0","MSXML2.XMLHttp"],
+			i,len;
+			for(i=0,len=versions.length;i<len;i++){
+				try{
+					new ActiveXObject(versions[i]);
+					arguments.callee.activeXString = versions[i];
+					break;
+				}
+				catch(ex){
+
+				}
+			}	
+		}	
+		return new ActiveXObject(arguments.callee.activeXString);
+	}else{
+		throw new Error("No XHR object availiable.");
+	}
+}
